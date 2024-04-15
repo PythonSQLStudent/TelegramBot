dd4b47029a814f3ea021f9e689b2149dfd3d924c
PythonSQLStudent <102534426+PythonSQLStudent@users.noreply.github.com>
2024-04-15 14:54:21 +0300

    Update main.py
    add weather

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import telebot
+import requests
+import json
 from configuration import API, swear_words, API_weather
+
 
 bot = telebot.TeleBot(API)
 
+
+#Погода
+
+@bot.message_handler(commands=['Погода'])
+def weather(message):
+    city = 'Moscow'
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_weather}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        bot.reply_to(message, f'Сейчас погода: {temp}\nОщущается как: {feels_like}')
+    else:
+        bot.reply_to(message, 'Город не найден')
 
 # Ругающийся бот
 
@@ -12,20 +29,10 @@ def main(message):
     Функция для ответа и подсчета ответов на "да" и "нет"
     """
     if message.text.strip().lower() == 'нет':
-        bot.send_message(message.chat.id, f'{message.text} - Пидора ответ')
+        bot.send_message(message.chat.id, 'Пидора ответ')
     elif message.text.strip().lower() == 'да':
         bot.reply_to(message, 'Пизда')
     # elif message.text
-
-
-#Погода
-
-@bot.message_handler(commands=['Погода'])
-def weather(message):
-    city = 'Moscow'
-    f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_weather}'
-
-
 
 
 bot.polling(none_stop=True)
