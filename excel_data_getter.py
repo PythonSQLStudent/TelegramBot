1d801efddd215705696bf5d6ebf2a88ef36ec455
PythonSQLStudent <102534426+PythonSQLStudent@users.noreply.github.com>
2024-04-23 16:48:08 +0300

    Update excel_data_getter.py
    rework method, add new method handler

--- excel_data_getter.py
+++ excel_data_getter.py
@@ -3,126 +3,148 @@ import pandas as pd
 import datetime as dt
 
 
-class ExcelFile:
+def get_value(filename) -> dict:
     """
-    Класс для взаимодействия с Excel-файлом
+    Функция для получения точечно необходимой информации из Excel - файла
     """
-    def __init__(self, filename):
-        self.filename = filename
-        self.workbook = None
+    workbook = openpyxl.load_workbook(filename, data_only=True)
 
-    def get_value(self) -> dict:
-        """
-        Функция для получения точечно необходимой информации из Excel - файла
-        """
-        self.workbook = openpyxl.load_workbook(self.filename, data_only=True)
+    sheet_parameters = workbook['Параметры']
+    sheet_invest = workbook['Расчет инвест. затрат']
+    sheet_chart_dl = workbook['Расчет графика ЛП']
+    sheet_recoverable_cost = workbook['Возмещаемые затраты']
 
-        sheet_parameters = self.workbook['Параметры']
-        sheet_invest = self.workbook['Расчет инвест. затрат']
-        sheet_chart_dl = self.workbook['Расчет графика ЛП']
-        sheet_recoverable_cost = self.workbook['Возмещаемые затраты']
+    data_dict = dict()
 
-        data_dict = dict()
+    # получаем проценты за финансирование
+    total_percent = 0
+    for row in sheet_invest.iter_rows(min_row=4, max_row=30):
+        cell = row[3]
+        if cell.value is not None:
+            total_percent += cell.value
+    data_dict['Итого проценты за финансирование'] = total_percent
 
-        # получаем проценты за финансирование
-        total_percent = 0
-        for row in sheet_invest.iter_rows(min_row=4, max_row=30):
-            cell = row[3]
-            if cell.value is not None:
-                total_percent += cell.value
-        data_dict['Итого проценты за финансирование'] = total_percent
+    ## даты оплаты оплаты поставщику, размер оплаты поставщику, проценты за финансирование
+    for row in sheet_invest.iter_rows(min_row=9, max_row=14):
+        cell = row[1]
+        if cell.value is not None and cell.value != 0:
+            data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' сумма'] = cell.value
+            data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' дата'] = sheet_invest.cell(row = cell.row, column=cell.column + 1).value
+            data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' проценты'] = sheet_invest.cell(row = cell.row, column=cell.column + 2).value
 
-        ## даты оплаты оплаты поставщику, размер оплаты поставщику, проценты за финансирование
-        for row in sheet_invest.iter_rows(min_row=9, max_row=14):
-            cell = row[1]
-            if cell.value is not None and cell.value != 0:
-                data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' сумма'] = cell.value
-                data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' дата'] = sheet_invest.cell(row = cell.row, column=cell.column + 1).value
-                data_dict[sheet_invest.cell(row = cell.row, column=cell.column - 1).value + ' проценты'] = sheet_invest.cell(row = cell.row, column=cell.column + 2).value
+    # получаем размер аванс
+    data_dict['Размер аванса'] = sheet_invest['B6'].value
 
-        # получаем размер аванс
-        data_dict['Размер аванса'] = sheet_invest['B6'].value
+    # поучаем дату аванса
+    data_dict['Дата аванса'] = sheet_invest['C6'].value
 
-        # поучаем дату аванса
-        data_dict['Дата аванса'] = sheet_invest['C6'].value
+    # получаем дату передачи
+    data_dict['Дата передачи'] = sheet_invest['C3'].value
 
-        # получаем дату передачи
-        data_dict['Дата передачи'] = sheet_invest['C3'].value
+    # получаем дату первого периода
+    data_dict['Дата первого периода'] = sheet_chart_dl['B5'].value
 
-        # получаем дату первого периода
-        data_dict['Дата первого периода'] = sheet_chart_dl['B5'].value
+    # получаем проценты первого периода
+    data_dict['Проценты первого периода'] = sheet_chart_dl['M5'].value
 
-        # получаем проценты первого периода
-        data_dict['Проценты первого периода'] = sheet_chart_dl['M5'].value
+    # получаем сумму возмещаемых затрат
+    total_cost = 0
+    for row in sheet_recoverable_cost.iter_rows(min_row=3):
+        cell = row[2]
+        if cell.value is not None:
+            total_cost += cell.value
+    data_dict['Возмещаемые затраты'] = total_cost
 
-        # получаем сумму возмещаемых затрат
-        total_cost = 0
-        for row in sheet_recoverable_cost.iter_rows(min_row=3):
-            cell = row[2]
-            if cell.value is not None:
-                total_cost += cell.value
-        data_dict['Возмещаемые затраты'] = total_cost
+    # получаем сумму ДЛ
+    total_dl = 0
+    for row in sheet_chart_dl.iter_rows(min_row=3):
+        cell = row[13]
+        if cell.value is not None:
+            total_dl += cell.value
+    data_dict['Сумма ДЛ'] = -total_dl
 
-        # получаем сумму ДЛ
-        total_dl = 0
-        for row in sheet_chart_dl.iter_rows(min_row=3):
-            cell = row[13]
-            if cell.value is not None:
-                total_dl += cell.value
-        data_dict['Сумма ДЛ'] = -total_dl
+    # получаем сумму процентов за период лизинга
+    total_percent = 0
+    for row in sheet_chart_dl.iter_rows(min_row=3):
+        cell = row[12]
+        if cell.value is not None:
+            total_percent += cell.value
+    data_dict['Сумма процентов за период лизинга'] = total_percent
 
-        # получаем сумму процентов за период лизинга
-        total_percent = 0
-        for row in sheet_chart_dl.iter_rows(min_row=3):
-            cell = row[12]
-            if cell.value is not None:
-                total_percent += cell.value
-        data_dict['Сумма процентов за период лизинга'] = total_percent
+    # получаем сумму субсидии
+    total_subs = 0
+    for row in sheet_chart_dl.iter_rows(min_row=3):
+        cell = row[14]
+        if cell.value is not None:
+            total_subs += cell.value
+    data_dict['Сумма субсидии за лизинг'] = -total_subs
 
-        # получаем сумму субсидии
-        total_subs = 0
-        for row in sheet_chart_dl.iter_rows(min_row=3):
-            cell = row[14]
-            if cell.value is not None:
-                total_subs += cell.value
-        data_dict['Сумма субсидии за лизинг'] = -total_subs
-
-        # получаем данные с листа параметров: Итого_ДКП_С_НДС, Отсрочка ДЛ, Срок ДЛ в мес
-        for row in sheet_parameters.iter_rows():
-            cell = row[1]
-            if cell.value == 'Отсрочка_ДЛ':
-                data_dict['Отсрочка ДЛ'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
-            elif cell.value == 'Срок_ДЛ_Мес':
-                data_dict['Срок ДЛ'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
-            elif cell.value == 'ИТОГО_ДКП_С_НДС':
-                data_dict['ИТОГО_ДКП_С_НДС'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
-
-
-        return data_dict
+    # получаем данные с листа параметров: Итого_ДКП_С_НДС, Отсрочка ДЛ, Срок ДЛ в мес
+    for row in sheet_parameters.iter_rows():
+        cell = row[1]
+        if cell.value == 'Отсрочка_ДЛ':
+            data_dict['Отсрочка ДЛ'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
+        elif cell.value == 'Срок_ДЛ_Мес':
+            data_dict['Срок ДЛ'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
+        elif cell.value == 'ИТОГО_ДКП_С_НДС':
+            data_dict['ИТОГО_ДКП_С_НДС'] = sheet_parameters.cell(row=cell.row, column=cell.column + 1).value
     
-    def close_wb(self):
-        """
-        Функция для закрытия Excel - файла
-        """
-        self.workbook.close()
+    workbook.close()
 
+
+    return data_dict
+    
+def answer(filename1, filename2=None):
+    """
+    Функция обработчик запроса из телеграм бота
+    filename1: имя Excel - файла с значениями 
+    filename2: имя второго Excel - файла для разбора инцидента с 2мя файлами
+    """
+    answer = 'Коллеги, добрый день,\n\nПо результатам рассмотрения инцидента могу сообщить следующее:\n\n'
+    dict1 = get_value(filename1)
+
+    # TODO: сделать для одного файла
+    if filename2 is None:
+        pass
+    else:
+        dict2 = get_value(filename2)
+        answer += f'Разница в сумме ДЛ составила {round(dict2['Сумма ДЛ'] - dict1['Сумма ДЛ'])} \n\n'
+
+        # TODO: описать проценты за финансирование
+        if dict2['Итого проценты за финансирование'] != dict1['Итого проценты за финансирование']:
+            answer += 'Проценты за финансирование\n\n'
+        
+        # TODO: описать изменение возмещаемых затрат
+        if dict2['Возмещаемые затраты'] != dict1['Возмещаемые затраты']:
+            answer += 'Возмещаемые затраты\n\n'
+
+        # TODO: описать изменение аванса
+        if dict2['Размер аванса'] != dict1['Размер аванса']:
+            answer += 'Размер аванса\n\n'
+
+        # TODO: описать изменение возмещаемых затрат
+        if dict2['Отсрочка ДЛ'] != dict1['Отсрочка ДЛ']:
+            answer += 'Отсрочка ДЛ\n\n'
+    
+    return answer
 
 filename_1 = 'по факту 302425785.xlsx'
 filename_2 = 'предварительный 302425787.xlsx'
-# abs_filename = os.path.abspath(filename)
-first_class = ExcelFile(filename_1)
-dict1 = first_class.get_value()
-print(dict1)
-first_class.close_wb()
+print(answer(filename_2, filename_1))
 
-sc_class = ExcelFile(filename_2)
-dict2 = sc_class.get_value()
-print(dict2)
-sc_class.close_wb()
+# first_class = ExcelFile(filename_1)
+# dict1 = first_class.get_value()
+# print(dict1)
+# first_class.close_wb()
 
-# dicts = [l, s]
-df1 = pd.DataFrame.from_dict(dict1, orient='index')
-df2 = pd.DataFrame.from_dict(dict2, orient='index')
+# sc_class = ExcelFile(filename_2)
+# dict2 = sc_class.get_value()
+# print(dict2)
+# sc_class.close_wb()
+
+# # dicts = [l, s]
+# df1 = pd.DataFrame.from_dict(dict1, orient='index')
+# df2 = pd.DataFrame.from_dict(dict2, orient='index')
 # keys = []
 # for i in dict1.keys():
 #     keys.append(i)
@@ -130,6 +152,6 @@ df2 = pd.DataFrame.from_dict(dict2, orient='index')
 # df = pd.DataFrame(dict1, index=dict1.keys()).join(pd.DataFrame(dict2, index=dict1.keys()))
 # df = pd.merge(df1, df2, on=keys, suffixes=['_df1', '_df2'])
 # df = pd.concat([pd.DataFrame.from_dict(d, orient='index') for d in dicts], ignore_index=True)
-df = pd.concat([df1, df2], axis=1)
-# print(df)
-df.to_excel("data.xlsx", index='False')
+# df = pd.concat([df1, df2], axis=1)
+# # print(df)
+# df.to_excel("data.xlsx", index='False')
